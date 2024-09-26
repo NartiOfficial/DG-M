a2c38c9318107a0744ceca39ae8a0226701ab9f4
Jakub Wielgocki <jakubwielgocki02@gmail.com>
2024-09-26 15:40:38 +0200

    Last fixes

--- js/main.js
+++ js/main.js
@@ -1,11 +1,24 @@
 const navMobile = document.querySelector(".nav-mobile");
 const navBtn = document.querySelector(".hamburger");
 const footerYear = document.querySelector(".footer__year");
+const body = document.querySelector("body");
+const navLinks = document.querySelectorAll(".nav-mobile a");
 
 const handleNav = () => {
   navBtn.classList.toggle("is-active");
   navMobile.classList.toggle("nav-mobile--active");
+  body.classList.toggle("no-scroll");
 };
+
+const closeNavOnClick = () => {
+  navMobile.classList.remove("nav-mobile--active");
+  navBtn.classList.remove("is-active");
+  body.classList.remove("no-scroll");
+};
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", closeNavOnClick);
+});
 
 navBtn.addEventListener("click", handleNav);
 
